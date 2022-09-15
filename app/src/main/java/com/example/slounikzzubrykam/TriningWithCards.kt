1ed8c1e90cb622f7f2723d3aa1837ea4002ce26d
panibeetle <panibeetle@gmail.com>
2022-09-15 12:26:59 +0200

    with first picture

--- app/src/main/java/com/example/slounikzzubrykam/TriningWithCards.kt
+++ app/src/main/java/com/example/slounikzzubrykam/TriningWithCards.kt
@@ -3,10 +3,9 @@ package com.example.slounikzzubrykam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TriningWithCards : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_trining_with_cards)
     }
 }
